99936e501f4474be6374d87ee38f903bcbb6c78e
Terence Parassi <naikatepa@gmail.com>
2020-04-17 09:24:27 +0300

    Added myxmltodict.py

---
+++ myxmltodict.py
@@ -0,0 +1,19 @@
+import xmltodict
+from pprint import pprint
+
+xml_example = open("xml_example.xml").read()
+pprint(xml_example)
+
+print("\n")
+
+xml_dict = xmltodict.parse(xml_example)
+int_name = xml_dict["interface"]["name"]
+pprint(int_name)
+
+print("\n")
+
+xml_data = xmltodict.unparse(xml_dict)
+pprint(xml_data)
+
+print("\n")
+
