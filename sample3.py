264dbc6fb7845f5249d51e0176349b5cc9aa2ef6
Terence Parassi <naikatepa@gmail.com>
2020-04-27 11:26:26 +0300

    Ammended Sample3.py to display ALL and 2nd person

--- sample3.py
+++ sample3.py
@@ -1,11 +1,25 @@
 import xmltodict
+from pprint import pprint
 
-#Get the XML file data
-stream = open('sample.xml','r')
+# Get the XML file data
+stream = open('sample.xml', 'r')
 
-#Parse the XML file into an 'OderedDict'
+# Parse the XML file into an 'OderedDict'
 xml = xmltodict.parse(stream.read())
+pprint(xml)
+print("\n")
+
+print("Display all the people in the list:")
+
 
 for e in xml["People"]["Person"]:
     print(e)
 
+print("\n")
+
+print("Display the second person in the list:")
+
+person = xml["People"]["Person"][1]
+print(person)
+
+print("\n")
